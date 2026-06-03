68a6728bf54ef0bc61915aa34ea79cfbb45ebb0b
RanadeepVinukonda <viranadeep@gmail.com>
2026-06-03 16:06:29 +0530

    fix: remove BrowserRouter nesting conflict with RouterProvider

--- lms/frontend/src/main.tsx
+++ lms/frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'sonner';
@@ -24,18 +23,16 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <App />
-          <Toaster
-            position="top-right"
-            richColors
-            closeButton
-            expand
-            duration={4000}
-          />
-        </QueryClientProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster
+          position="top-right"
+          richColors
+          closeButton
+          expand
+          duration={4000}
+        />
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>,
 );
